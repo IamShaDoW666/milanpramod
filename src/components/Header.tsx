d71a17dc9ce17e872f0bac75c64e706bc2d89c65
IamShaDoW666 <www.gaming2018@gmail.com>
2023-02-01 01:43:55 +0530

    new profile + updated animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,12 +10,39 @@ const Header: React.FC = () => {
   const socialRef = useRef<HTMLDivElement>(null);
   const nameRef = useRef<HTMLHeadingElement>(null);
   const titleRef = useRef<HTMLHeadingElement>(null);
+  const spanRef = useRef<HTMLHeadingElement>(null);
+  const spannRef = useRef<HTMLHeadingElement>(null);
 
   useEffect(() => {
-    gsap.to(nameRef.current, {opacity: 1, duration: 1, delay: 0.2})
+    gsap.to(nameRef.current, { opacity: 1, duration: 1, delay: 0.2 });
     let tl = gsap.timeline();
-    tl.fromTo(textRef.current, {opacity: 0, y: 16}, {opacity: 1, duration: 1, y: 0, ease: 'easeIn'})
-    tl.fromTo(socialRef.current, {opacity: 0, y: -16}, {opacity: 1, duration: 1, y: 0, ease: 'easeOut'})
+    tl.fromTo(
+      textRef.current,
+      { opacity: 0, y: 16 },
+      { opacity: 1, duration: 1, y: 0, ease: "easeIn", onComplete: () => {
+        gsap.fromTo(
+          spanRef.current,
+          { backgroundColor: "" },
+          {
+            duration: 0.7,
+            backgroundColor: "#ED5B87",
+            ease: "easeIn",
+            onStart: () => {
+              gsap.fromTo(
+                spannRef.current,
+                { backgroundColor: "" },
+                { duration: 0.7, delay: 0.2, backgroundColor: "#ED5B87", ease: "easeIn" }
+              );
+            },
+          }
+        );
+      }}
+    );
+    tl.fromTo(
+      socialRef.current,
+      { opacity: 0, y: -16 },
+      { opacity: 1, duration: 1, y: 0, ease: "easeOut" }
+    );    
   }, []);
   return (
     <div className="sm:flex font-mono">
@@ -31,7 +58,9 @@ const Header: React.FC = () => {
           />
         </div>
         <div className="mt-4">
-          <h1 ref={nameRef} className="font-semibold opacity-0 text-xl">Milan Pramod</h1>
+          <h1 ref={nameRef} className="font-semibold opacity-0 text-xl">
+            Milan Pramod
+          </h1>
           <h4 className="text-sm text-center text-primary font-semibold">
             Student
           </h4>
@@ -43,9 +72,9 @@ const Header: React.FC = () => {
           className="px-6 opacity-0 md:text-lg lg:text-xl leading-loose tracking-wide font-semibold"
         >
           Hi, I am Milan Pramod. I am a self taught{" "}
-          <span className="bg-primary">full-stack developer</span> and{" "}
-          <span className="bg-primary">UI/UX Designer.</span> I love
-          contributing to open source. I look forward to make your ideas real.
+          <span ref={spanRef}>full-stack developer</span> and{" "}
+          <span ref={spannRef}>UI/UX Designer.</span> I love contributing to
+          open source. I look forward to make your ideas real.
         </p>
         <div ref={socialRef} className="flex space-x-10 mt-8 px-6">
           <FacebookIcon
@@ -54,8 +83,16 @@ const Header: React.FC = () => {
             width={48}
             height={48}
           />
-          <InstagramIcon href="https://google.com" width={48} height={48} />
-          <GithubIcon href="https://google.com" width={48} height={48} />
+          <InstagramIcon
+            href="https://instagram.com/___el_diablo___?igshid=ZDdkNTZiNTM="
+            width={48}
+            height={48}
+          />
+          <GithubIcon
+            href="https://github.com/IamShaDoW666"
+            width={48}
+            height={48}
+          />
         </div>
       </div>
     </div>
